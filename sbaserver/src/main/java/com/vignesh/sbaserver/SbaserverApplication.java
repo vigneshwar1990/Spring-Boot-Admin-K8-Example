998480932f29b010fd9d29ef55cc70805b203717
vigneshwarr1990 <vigneshwarr1990>
2020-02-15 21:48:34 -0600

    Initial Working Changes - Instances up

--- sbaserver/src/main/java/com/vignesh/sbaserver/SbaserverApplication.java
+++ sbaserver/src/main/java/com/vignesh/sbaserver/SbaserverApplication.java
@@ -4,10 +4,13 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-@EnableAdminServer
+
 @EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+@EnableScheduling
 public class SbaserverApplication {
 
 	public static void main(String[] args) {
